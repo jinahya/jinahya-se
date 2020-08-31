b4ba817367e773e63d7c253a46c1cfe594cf2c42
Jin Kwon <onacit@gmail.com>
2020-08-31 20:41:48 +0900

    #8 Add JinahyaCollections#requireNonEmpty(Collection)

--- src/main/java/com/github/jinahya/util/JinahyaCollections.java
+++ src/main/java/com/github/jinahya/util/JinahyaCollections.java
@@ -5,6 +5,21 @@ import java.util.Collections;
 
 public final class JinahyaCollections {
 
+    /**
+     * Checks that specified collection is not {@link Collection#isEmpty() empty}. Note that this method passes with
+     * {@code null} argument.
+     *
+     * @param collection the collection to check.
+     * @param <T>        collection type parameter
+     * @return {@code collection} is not empty.
+     */
+    public static <T extends Collection<?>> T requireNonEmpty(final T collection) {
+        if (collection != null && !collection.isEmpty()) {
+            throw new IllegalArgumentException("collection(" + collection + ") is empty");
+        }
+        return collection;
+    }
+
     public static <T extends Collection<? super E>, E> T addAll(final T c, final E... elements) {
         final boolean changed = Collections.addAll(c, elements);
         return c;
